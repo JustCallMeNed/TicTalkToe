0392cb80cd09ba28f90f3145b7f4b76d71e798a3
JustCallMeNed <neal.c.fisher@gmail.com>
2022-01-09 17:18:30 -0600

    minor tweaks to 'Game.js', created 'Game.css

--- tic-talk-toe/src/pages/Game.js
+++ tic-talk-toe/src/pages/Game.js
@@ -1,6 +1,6 @@
 const Query = () => {
   return (
-    <div>
+    <div id="gameBoard">
       <div id="topRow">
         <select class="cords" id="cord1">
           <option>_</option>
@@ -12,41 +12,41 @@ const Query = () => {
           <option>X</option>
           <option>O</option>
         </select>
-        <select class="cords" id="location3">
+        <select class="cords" id="cord3">
           <option>_</option>
           <option>X</option>
           <option>O</option>
         </select>
       </div>
       <div id="middleRow">
-        <select class="cords" id="location4">
+        <select class="cords" id="cord4">
           <option>_</option>
           <option>X</option>
           <option>O</option>
         </select>
-        <select class="cords" id="location5">
+        <select class="cords" id="cord5">
           <option>_</option>
           <option>X</option>
           <option>O</option>
         </select>
-        <select class="cords" id="location6">
+        <select class="cords" id="cord6">
           <option>_</option>
           <option>X</option>
           <option>O</option>
         </select>
       </div>
       <div id="bottomRow">
-        <select class="cords" id="location7">
+        <select class="cords" id="cord7">
           <option>_</option>
           <option>X</option>
           <option>O</option>
         </select>
-        <select class="cords" id="location8">
+        <select class="cords" id="cord8">
           <option>_</option>
           <option>X</option>
           <option>O</option>
         </select>
-        <select class="cords" id="location9">
+        <select class="cords" id="cord9">
           <option>_</option>
           <option>X</option>
           <option>O</option>
