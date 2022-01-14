cafea5148142d7c5caaad9f27e92733471c00fdf
JustCallMeNed <neal.c.fisher@gmail.com>
2022-01-14 00:36:28 -0600

    started in on logic for CordBox.js, Game.js - unsure how to proceed just yet.

--- tic-talk-toe/src/pages/Game/components/CordBox.js
+++ tic-talk-toe/src/pages/Game/components/CordBox.js
@@ -1,11 +1,28 @@
+import React, { useState, useRef, useEffect } from "react";
 const CordBox = ({ cordIndex }) => {
+  const [submit, setSubmit] = useState(false);
+  const [marked, setMarked] = useState(false);
+
+  // Logic for each Cord box to know when it's been "marked"
+  useEffect(() => {
+    console.log(`cord${cordIndex}`.value);
+    if (`cord${cordIndex}`?.value !== undefined) {
+      setMarked(true);
+    } else {
+      setMarked(false);
+      console.log(`cord${cordIndex}`?.value, marked);
+    }
+  }, [submit]);
   return (
-    <select className="cords" id={`cord${cordIndex}`}>
+    <select
+      className="cords"
+      id={`cord${cordIndex}`}
+      onChange={() => setSubmit(!submit)}
+    >
       <option>_</option>
       <option>X</option>
       <option>O</option>
     </select>
   );
 };
-
 export default CordBox;
