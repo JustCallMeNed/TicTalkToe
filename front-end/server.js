f194d95e1d94bd61b23f750755bed47f9a494654
Val-code-1 <reit.ben@gmail.com>
2022-01-16 18:45:32 -0600

    Sequelize

---
+++ front-end/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const db = require("./models");
+const http = require("http");
+const server = http.createServer(app);
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const path = require("path");
+const port = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/dist")));
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Now THIS is pod racing");
+});
+
+// routes
+require("./routes/api-routes.js")(app);
+
+db.sequelize.sync().then(() => {
+  app.listen(port, () => {
+    console.log(`App is listening on port: ${port}`);
+  });
+});
