7420c1b9601538ce4822c66a8181d9ef2ecb6398
JustCallMeNed <neal.c.fisher@gmail.com>
2022-01-22 21:12:58 -0600

    finished board-reset function, cleaned up component files, removed 'refresh', 'mark' constants and replaced with coordinates

--- src/pages/Game/components/CordBox.js
+++ src/pages/Game/components/CordBox.js
@@ -1,44 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 const CordBox = ({
   turn,
   setTurn,
   ticBoard,
   setTicBoard,
-  cords,
   cordIndex,
   rowIndex,
   boxIndex,
-  refresh,
-  setRefresh,
 }) => {
-  const [mark, setMark] = useState("");
-  // useEffect(() => {
-  //   if (refresh === true) {
-  //     setMark("");
-  //   }
-  //   return setRefresh(false);
-  // }),
-  //   [];
-  // pass "refresh" value from Game.js, reset mark to empty string?
-
   return (
     <div
       className="cords"
       id={`cord${cordIndex}`}
       onClick={() => {
-        if (mark === "") {
+        if (ticBoard[rowIndex][boxIndex] === "") {
           let copy = [...ticBoard];
           copy[rowIndex][boxIndex] = turn;
           setTicBoard(copy);
-          setMark(turn);
           setTurn(turn === "X" ? "O" : "X");
-        } else if (cords.currentState !== "" || refresh === true) {
-          setMark("");
         }
       }}
-      currentState={mark}
+      currentState={ticBoard[rowIndex][boxIndex]}
     >
-      {mark}
+      {ticBoard[rowIndex][boxIndex]}
     </div>
   );
 };
