a356229b255fcb7370bc940eb0539756fe43fcc0
gagnet <tim.madison.gagne@gmail.com>
2022-01-22 19:07:46 -0800

    for neal to push

--- back-end/migrations/create-user.js
+++ back-end/migrations/create-user.js
@@ -14,72 +14,13 @@ module.exports = {
       email: {
         type: Sequelize.STRING,
         allowNull: false,
+        // has to have an email
         unique: true,
+        // can't be the same email
       },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
-        // defaultValue: Sequelize.NOW,
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        // defaultValue: Sequelize.NOW,
-      },
-    });
-
-    await queryInterface.createTable("channel", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      name: {
-        type: Sequelize.STRING,
-      },
-      user_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: "user",
-          key: "id",
-        },
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        // defaultValue: Sequelize.NOW,
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        // defaultValue: Sequelize.NOW,
-      },
-    });
-
-    await queryInterface.createTable("video", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      title: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      channel_id: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: "channel",
-          key: "id",
-          allowNull: false,
-        },
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
         // defaultValue: Sequelize.NOW,
       },
       updated_at: {
