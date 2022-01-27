ff25ebcee3e3d2ee7d9cb265236707eb0c59acf6
Val-code-1 <reit.ben@gmail.com>
2022-01-27 01:20:37 -0600

    Added new user signup modal

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,15 +2,32 @@ import "./Login.css";
 import axios from "axios";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-// import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 import { useState } from "react";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  zIndex: "1000",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Login = () => {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <Box className="loginPage">
@@ -30,6 +47,7 @@ const Login = () => {
               id="name"
               label="Username"
               variant="outlined"
+              autoComplete="off"
               name="name"
               required
               minlength="4"
@@ -47,6 +65,7 @@ const Login = () => {
               id="pass"
               label="Password"
               variant="outlined"
+              autoComplete="off"
               name="pass"
               required
               minlength="8"
@@ -88,6 +107,85 @@ const Login = () => {
           </Grid>
         </Grid>
       </form>
+      {/* SignUp */}
+      <Box>
+        <Button onClick={handleOpen}>New User? Click Here!</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Enter New Username and Password
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              <form>
+                <Grid container spacing={2} columns={1} direction="column">
+                  <Grid item spacing={1} className="username">
+                    <TextField
+                      type="text"
+                      onChange={(e) => {
+                        setUserName(e.target.value);
+                      }}
+                      id="name"
+                      label="Username"
+                      variant="outlined"
+                      autoComplete="off"
+                      name="name"
+                      required
+                      minlength="4"
+                      maxlength="20"
+                      size="20"
+                    ></TextField>
+                  </Grid>
+                  <Grid item spacing={1} className="password">
+                    <TextField
+                      type="password"
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                      id="pass"
+                      label="Password"
+                      variant="outlined"
+                      autoComplete="off"
+                      name="pass"
+                      required
+                      minlength="8"
+                      maxlength="20"
+                      size="20"
+                    ></TextField>
+                  </Grid>
+                  <br />
+                </Grid>
+                <Grid container rowSpacing={1} columnSpacing={{ sm: 2 }}>
+                  <Grid item>
+                    <Button
+                      variant="contained"
+                      id="loginSubmit"
+                      onClick={() => {
+                        console.log(userName, password);
+                        axios.post("http://localhost:3001/user/create", {
+                          userName,
+                          password,
+                        });
+                      }}
+                    >
+                      Submit
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant="contained" id="loginSubmit" type="reset">
+                      Clear
+                    </Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </Typography>
+          </Box>
+        </Modal>
+      </Box>
     </Box>
   );
 };
