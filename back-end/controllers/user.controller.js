68e3a9822a25922f6b239e6c6a87c7db87abe9b1
gagnet <tim.madison.gagne@gmail.com>
2022-01-27 16:30:30 -0800

    the get function is now working? at least I think?

--- back-end/controllers/user.controller.js
+++ back-end/controllers/user.controller.js
@@ -18,3 +18,90 @@ exports.create = (req, res) => {
     });
   console.log(req.body);
 };
+exports.findAll = (req, res) => {
+  const userName = req.query.userName;
+  var condition = userName
+    ? { userName: { [Op.like]: `%${userName}%` } }
+    : null;
+
+  User.findAll({ where: condition })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while finding the Gig.",
+      });
+    });
+  console.log(this.findAll);
+};
+
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  User.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num === 1) {
+        res.send({
+          message: "User was updated successfully.",
+        });
+      } else {
+        res.send({
+          message: `Cannot update User with id=${id}. Maybe User was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating User with id=" + id,
+      });
+    });
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  User.destroy({
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num === 1) {
+        res.send({
+          message: "User was deleted successfully!",
+        });
+      } else {
+        res.send({
+          message: `Cannot delete User with id=${id}. Maybe Tutorial was not found!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete User with id=" + id,
+      });
+    });
+};
+
+// Find all published Tutorials
+
+// exports.get = (req, res) => {
+//   const user = {
+//     username: req.body.userName,
+//     password: req.body.password,
+//   };
+//   User.get(user)
+//     .then((data) => {
+//       res.send(data);
+//     })
+//     .catch((err) => {
+//       res.status(500).send({
+//         message: err.message || "Some error occurred while reading the Gig.",
+//       });
+//     });
+//   console.log(req.body);
+// };
+
+// current login page is only creating users. Isn't fucntioning as reading existing users from the table.
+//  get post put delete
